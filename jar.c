7249ddfe919a61b577a27e99631d08b821c8742d
Techdockttor <diogu.ugochukwu@gmail.com>
2023-09-19 16:22:33 -0700

    jar.c

--- jar.c
+++ jar.c
@@ -3,39 +3,6 @@
 #include <unistd.h>
 
 /**
- * custom_putchar - Write a character to the standard output
- * @c: The character to be written
- * Return: 1 on success, -1 on error
- */
-int custom_putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * custom_puts - Write a string to the standard output
- * @s: The string to be written
- * Return: The number of characters written
- */
-int custom_puts(char *s)
-{
-	int custom_puts(char *s);
-	int count = 0;
-
-	if (!s)
-	return (custom_puts("(null)"));
-
-	while (*s)
-	{
-		if (custom_putchar(*s) == -1)
-			return (-1);
-		count++;
-		s++;
-	}
-	return (count);
-}
-
-/**
  * _printf - produces output according to a format
  * @format: Character string that directs the format of the output
  * Return: The number of characters printed, excluding the null byte
@@ -44,60 +11,60 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	char buffer[20];
-	int count = 0, i = 0;
+	int count = 0, z = 0;
 
 	va_start(args, format);
-	while (format[i])
+	while (format[z])
 	{
-		if (format[i] == '%' && format[i + 1])
+		if (format[z] == '%' && format[z + 1])
 		{
-			i++;
-			if (format[i] == 'c')
+			z++;
+			if (format[z] == 'c')
 				count += custom_putchar(va_arg(args, int));
-			else if (format[i] == 's')
+			else if (format[z] == 's')
 			{
 				char *s = va_arg(args, char *);
 
 				s = s ? s : "(null)";
 				count += custom_puts(s);
 			}
-			else if (format[i] == 'i' || format[i] == 'd')
+			else if (format[z] == 'i' || format[z] == 'd')
 			{
 				snprintf(buffer, sizeof(buffer), "%d", va_arg(args, int));
 				count += custom_puts(buffer);
 			}
-			else if (format[i] == 'u')
+			else if (format[z] == 'u')
 			{
 				snprintf(buffer, sizeof(buffer), "%u", va_arg(args, unsigned int));
 				count += custom_puts(buffer);
 			}
-			else if (format[i] == 'o')
+			else if (format[z] == 'o')
 			{
 				snprintf(buffer, sizeof(buffer), "%o", va_arg(args, unsigned int));
 				count += custom_puts(buffer);
 			}
-			else if (format[i] == 'x')
+			else if (format[z] == 'x')
 			{
 				snprintf(buffer, sizeof(buffer), "%x", va_arg(args, unsigned int));
 				count += custom_puts(buffer);
 			}
-			else if (format[i] == 'X')
+			else if (format[z] == 'X')
 			{
 				snprintf(buffer, sizeof(buffer), "%X", va_arg(args, unsigned int));
 				count += custom_puts(buffer);
 			}
-			else if (format[i] == '%')
+			else if (format[z] == '%')
 				count += custom_putchar('%');
 			else
 			{
 				count += custom_putchar('%');
-				count += custom_putchar(format[i]);
+				count += custom_putchar(format[z]);
 			}
 		}
 		else
 		{
-			count += custom_putchar(format[i]);
-		i++;
+			count += custom_putchar(format[z]);
+		z++;
 		}
 	}
 
